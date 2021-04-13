da33569498fe269b72a928fe01e5cff2dbc19c54
marianaga2100 <82132215+marianaga2100@users.noreply.github.com>
2021-04-13 08:11:36 -0500

    Update WebConfig.java

--- src/main/java/org/marian/garciaa/config/WebConfig.java
+++ src/main/java/org/marian/garciaa/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer{
 	public void addResourceHandlers(
             ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/logos/**").
-        addResourceLocations("file:c:/empleos/img-vacantes/");
+        addResourceLocations("file:img-vacantes");
     }
 
 }
